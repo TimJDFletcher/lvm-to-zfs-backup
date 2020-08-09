186cbc8f4164cc964d0dda4122364263316dbd9a
Tim Fletcher <tim@night-shade.org.uk>
2020-08-09 14:34:00 +0200

    Remove dead code, update scripts using shell check

--- zfs-backup.sh
+++ zfs-backup.sh
@@ -1,102 +1,99 @@
-#!/bin/bash -e
+#!/bin/bash
+set -eu -o pipefail
+
 . /etc/zfsbackups/zfs-backup.conf
 
-if [ $(id -u) -gt 0 ] ; then
-    echo $0 needs to be run as root
-    exit 1
+if [ $UID -gt 0 ]; then
+  echo "$0" needs to be run as root
+  exit 1
 fi
 
-vglock()
-{
-    if [ -f $lockfile ] ; then
-        echo $lockfile found, bailing out
-        logger ZFS backup aborted, lock file found
-        break
-    else
-        logger ZFS backup started
-        touch $lockfile
-    fi
+vglock() {
+  if [ -f "$lockfile" ]; then
+    echo "$lockfile" found, bailing out
+    logger ZFS backup aborted, lock file found
+    return 1
+  else
+    logger ZFS backup started
+    touch "$lockfile"
+  fi
 }
 
-vgunlock()
-{
-    if [ -f $lockfile ] ; then
-        rm $lockfile
-    fi
+vgunlock() {
+  rm -f "$lockfile"
 }
 
-vgbackup()
-{
-    backupdir=/$TARGET/lvm/$vg
-    echo "Backing up volume group $vg"
-    for volume in $(lvm lvs --noheadings -o lv_name $vg) ; do
-        if mount | grep -q "^/dev/mapper/${vg}-${volume} "; then
-            echo "$volume"
-            # Take an LVM snapshot 10% of the size of the origin volume
-            lvm lvcreate --quiet --extents 10%ORIGIN --chunksize 512k --snapshot --name ${volume}.${DATE} /dev/${vg}/${volume}
-            blockdev --setro /dev/${vg}/${volume}.${DATE}
-            mkdir -p $SNAP_MOUNTPOINT/$DATE/$volume
-            if [ -f $CONF_DIR/${vg}-${volume}.excludes ] ; then
-                rsyncargs="${RSYNC_ARGS_BASE} --delete-excluded --exclude-from=${CONF_DIR}/${vg}-${volume}.excludes"
-            else
-                rsyncargs="${RSYNC_ARGS_BASE}"
-            fi
-            # Actually backup files
-            if mount -o ro /dev/${vg}/${volume}.${DATE} $SNAP_MOUNTPOINT/$DATE/$volume ; then
-                mkdir -p $backupdir/$volume
-                $RSYNC_CMD $rsyncargs $SNAP_MOUNTPOINT/$DATE/$volume/ $backupdir/$volume/
-                umount $SNAP_MOUNTPOINT/$DATE/$volume
-            else
-                echo "$volume snapshot failed to mount skipping backup"
-            fi
-            sync ; sleep 10
-            if ! lvm lvremove --quiet --force ${vg}/${volume}.${DATE} ; then
-                echo lvremove failed, sleeping 30 seconds and using dmsetup
-                sync ; sleep 30
-                dmsetup remove ${vg}-${volume}-real
-                dmsetup remove ${vg}-${volume}.${DATE}
-                lvm lvremove --quiet --force ${vg}/${volume}.${DATE}
-            fi
-            rmdir $SNAP_MOUNTPOINT/$DATE/$volume
-        else
-            echo "$volume not mounted skipping backup"
-        fi
-    done
-    rmdir $SNAP_MOUNTPOINT/$DATE
-    echo done
-}
-
-mountpointbackup()
-{
-    backupdir=$TARGET/extra
-    mkdir -p $backupdir
-    echo -n "$(basename $mountpoint), "
-    if [ "x/" = "x$mountpoint" ] ; then
-        safename=root
-    else
-        safename=$(echo $mountpoint | sed -e s,^/,,g -e s,/,.,g )
-    fi
-    if [ -f $CONF_DIR/${safename}.excludes ] ; then
-        rsyncargs="${RSYNC_ARGS_BASE} --delete-excluded --exclude-from=${CONF_DIR}/${safename}.excludes"
-    else
+vgbackup() {
+  backupdir=/$TARGET/lvm/$vg
+  echo "Backing up volume group $vg"
+  for volume in $(lvm lvs --noheadings -o lv_name "$vg"); do
+    if grep -q "^/dev/mapper/${vg}-${volume} " /proc/mounts; then
+      echo "$volume"
+      # Take an LVM snapshot 10% of the size of the origin volume
+      lvm lvcreate --quiet --extents 10%ORIGIN --chunksize 512k --snapshot --name ${volume}.${DATE} /dev/${vg}/${volume}
+      blockdev --setro "/dev/${vg}/${volume}.${DATE}"
+      mkdir -p "$SNAP_MOUNTPOINT/$DATE/$volume"
+      if [ -f "$CONF_DIR/${vg}-${volume}.excludes" ]; then
+        rsyncargs="${RSYNC_ARGS_BASE} --delete-excluded --exclude-from=${CONF_DIR}/${vg}-${volume}.excludes"
+      else
         rsyncargs="${RSYNC_ARGS_BASE}"
+      fi
+      # Actually backup files
+      if mount -o ro "/dev/${vg}/${volume}.${DATE}" "${SNAP_MOUNTPOINT}/$DATE/$volume"; then
+        mkdir -p "$backupdir/$volume"
+        $RSYNC_CMD $rsyncargs "${SNAP_MOUNTPOINT}/${DATE}/$volume/" "$backupdir/$volume/"
+        umount "${SNAP_MOUNTPOINT}/${DATE}/$volume"
+      else
+        echo "$volume snapshot failed to mount skipping backup"
+      fi
+      sync ; sleep 10
+      if ! lvm lvremove --quiet --force "${vg}/${volume}.${DATE}"; then
+        echo lvremove failed, sleeping 30 seconds and using dmsetup
+        sync
+        sleep 30
+        dmsetup remove "${vg}-${volume}-real"
+        dmsetup remove "${vg}-${volume}.${DATE}"
+        lvm lvremove --quiet --force "${vg}/${volume}.${DATE}"
+      fi
+      rmdir "${SNAP_MOUNTPOINT}/${DATE}/$volume"
+    else
+      echo "$volume not mounted skipping backup"
     fi
-    if grep -q " $mountpoint " /proc/mounts ; then
-        $RSYNC_CMD $rsyncargs $mountpoint/ /$backupdir/$safename/
-    fi
+  done
+  rmdir "${SNAP_MOUNTPOINT}/${DATE}"
+  echo done
+}
+
+mountpointbackup() {
+  backupdir=/$TARGET/extra
+  mkdir -p $backupdir
+  echo -n "$(basename $mountpoint), "
+  if [ "x/" = "x$mountpoint" ]; then
+    safename=root
+  else
+    safename="$(echo $mountpoint | sed -e s,^/,,g -e s,/,.,g)"
+  fi
+  if [ -f "$CONF_DIR/${safename}.excludes" ]; then
+    rsyncargs="${RSYNC_ARGS_BASE} --delete-excluded --exclude-from=${CONF_DIR}/${safename}.excludes"
+  else
+    rsyncargs="${RSYNC_ARGS_BASE}"
+  fi
+  if grep -q " $mountpoint " /proc/mounts; then
+    $RSYNC_CMD "$rsyncargs $mountpoint/" "$backupdir/$safename/"
+  fi
 }
 
 echo "Backing up volume groups"
-for vg in $VGs ; do
-    lockfile=$lockdir/${vg}.zfsbackup
-    vglock
-    vgbackup
-    vgunlock
+for vg in ${VGs}; do
+  lockfile="${lockdir}/${vg}.zfsbackup"
+  vglock
+  vgbackup
+  vgunlock
 done
 
 echo -n "Backing up other filesystems: "
-for mountpoint in $EXTRAMOUNTPOINTS ; do
-    mountpointbackup
+for mountpoint in $EXTRAMOUNTPOINTS; do
+  mountpointbackup
 done
 echo done
 
